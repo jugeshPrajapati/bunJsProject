53fc0b43562acf8211117002c260d7b25330c77f
Jugesh <prajapatijugesh@gmail.com>
2024-07-18 12:28:35 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { type Request, type Response, type NextFunction, type Express } from 'express';
+const app: Express = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 8080;
+
+app.get(
+  '/',
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      res.status(200).json({
+        message: 'Hurray!! we create our first server on bun js hot updated ',
+      });
+    } catch (error) { next(error); }
+
+  },
+);
+
+app.listen(port, () => {
+  console.log(`Server is up and running on port localhost:${port}`);
+});
